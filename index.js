a32fd1b386ef75e08ba64d6a50e7987a72150037
Ivan Kishko1~ <oknavi779@gmail.com>
2024-05-03 19:41:40 +0200

    HW

---
+++ index.js
@@ -0,0 +1,17 @@
+// TASK 1
+let result = 5 + 5 + "5";
+console.log(result);
+// TASK 2
+let email = "exampleName@email.com";
+console.log(email.includes("@"));
+// TASK 3
+let myString = "My";
+let nameString = "name";
+let isString = "is";
+let fullName = `${myString} ${nameString} ${isString} Viktor`;
+console.log(fullName);
+// TASK 4
+let userName = "Олександро";
+let payment = 300;
+let paymentMessage = `Дякуємо, ${userName}! До сплати ${payment} гривень`;
+alert(paymentMessage);
